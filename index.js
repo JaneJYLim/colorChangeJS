6d9b3079c4a88656f0dda8defef58833a208b010
JaneJYLim <juyeonsh412@gmail.com>
2020-08-07 00:37:41 +0900

    color change v.1.0

---
+++ index.js
@@ -0,0 +1,20 @@
+
+let width = document.querySelector("body");
+
+const RESIZE_CLASS = "resize";
+
+function widthResize() {
+    
+    const hasClass = width.classList.contains(RESIZE_CLASS);
+    if (hasClass) {
+      width.classList.(RESIZE_CLASS);
+    } else {
+      width.classList.add(RESIZE_CLASS);
+    }
+
+}
+
+function init() {
+  window.addEventListener("resize", widthResize);
+}
+init();
